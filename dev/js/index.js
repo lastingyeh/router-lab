5fdd328e2133ed2462767361aac00a734850872d
lastingyeh <lastingyeh@gmail.com>
2016-12-11 14:29:12 +0800

    update index.js
    delete extra import

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,32 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
-// import {Provider} from 'react-redux';
-// import {createStore, applyMiddleware} from 'redux';
-// import thunk from 'redux-thunk';
-// import promise from 'redux-promise';
-// import createLogger from 'redux-logger';
-// import allReducers from './reducers';
-// import App from './components/App';
-
-// const logger = createLogger();
-// const store = createStore(
-//     allReducers,
-//     applyMiddleware(thunk, promise, logger)
-// );
-
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <App />
-//     </Provider>,
-//     document.getElementById('root')
-// );
-
-//Router test...
-//*****************************************
 
 import Main from './router-ex/lessons13';
-//import Main from './router-ex/router-lab';
 
 ReactDOM.render(<Main />,
     document.getElementById('root')
